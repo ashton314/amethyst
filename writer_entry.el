b68c5100f9631b611f2bfc89e9cdffabee8288df
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2020-12-11 18:40:50 -0700

    Add license; fold in sanity.el

--- writer_entry.el
+++ writer_entry.el
@@ -1,7 +1,46 @@
 ;; Entry point for my writer-focused mode
-;; Ashton Wiersdorf
+;; Copyright 2020 Ashton Wiersdorf
+;;
+;; Sizable portions of this code are derived from the elegance theme
+;; by Nicolas P. Rougier. Modifications made starting 8 December 2020.
+;;
+;; -------------------------------------------------------------------
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>
+;; -------------------------------------------------------------------
 
-(load-file "sanity.el")
+;; Nice defaults, snarfed from sanity.el
+(setq gc-cons-threshold (* 100 1024 1024))
+(setq inhibit-startup-screen t)
+(setq inhibit-startup-echo-area-message t)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+(setq initial-major-mode 'org-mode)
+(setq-default indent-tabs-mode nil)
+(setq pop-up-windows nil)
+(tool-bar-mode 0)
+(tooltip-mode  0)
+(scroll-bar-mode 0)
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+(save-place-mode 1)
+
+(global-set-key (kbd "C-z") 'undo)
+
 (load-file "elegance.el")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
